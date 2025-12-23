5d377213053d05d8951acf8aec4a89214d04bd9f
Leonardo Catananti <leonardo.catananti@gmail.com>
2025-12-23 11:22:08 -0300

    feat: add FaixaPremiacao and GanhadorUF embedded entities

---
+++ src/main/java/br/com/loterias/domain/entity/FaixaPremiacao.java
@@ -0,0 +1,90 @@
+package br.com.loterias.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "faixa_premiacao")
+public class FaixaPremiacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "concurso_id", nullable = false)
+    @JsonIgnore
+    private Concurso concurso;
+
+    @Column(nullable = false)
+    private Integer faixa;
+
+    @Column(name = "descricao_faixa")
+    private String descricaoFaixa;
+
+    @Column(name = "numero_ganhadores")
+    private Integer numeroGanhadores;
+
+    @Column(name = "valor_premio", precision = 19, scale = 2)
+    private BigDecimal valorPremio;
+
+    public FaixaPremiacao() {
+    }
+
+    public FaixaPremiacao(Integer faixa, String descricaoFaixa, Integer numeroGanhadores, BigDecimal valorPremio) {
+        this.faixa = faixa;
+        this.descricaoFaixa = descricaoFaixa;
+        this.numeroGanhadores = numeroGanhadores;
+        this.valorPremio = valorPremio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Concurso getConcurso() {
+        return concurso;
+    }
+
+    public void setConcurso(Concurso concurso) {
+        this.concurso = concurso;
+    }
+
+    public Integer getFaixa() {
+        return faixa;
+    }
+
+    public void setFaixa(Integer faixa) {
+        this.faixa = faixa;
+    }
+
+    public String getDescricaoFaixa() {
+        return descricaoFaixa;
+    }
+
+    public void setDescricaoFaixa(String descricaoFaixa) {
+        this.descricaoFaixa = descricaoFaixa;
+    }
+
+    public Integer getNumeroGanhadores() {
+        return numeroGanhadores;
+    }
+
+    public void setNumeroGanhadores(Integer numeroGanhadores) {
+        this.numeroGanhadores = numeroGanhadores;
+    }
+
+    public BigDecimal getValorPremio() {
+        return valorPremio;
+    }
+
+    public void setValorPremio(BigDecimal valorPremio) {
+        this.valorPremio = valorPremio;
+    }
+}
---
+++ src/main/java/br/com/loterias/domain/entity/GanhadorUF.java
@@ -0,0 +1,97 @@
+package br.com.loterias.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ganhador_uf")
+public class GanhadorUF {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "concurso_id", nullable = false)
+    @JsonIgnore
+    private Concurso concurso;
+
+    @Column(length = 2)
+    private String uf;
+
+    private String cidade;
+
+    @Column(name = "numero_ganhadores")
+    private Integer numeroGanhadores;
+
+    private Integer faixa;
+
+    private String canal;
+
+    public GanhadorUF() {
+    }
+
+    public GanhadorUF(String uf, String cidade, Integer numeroGanhadores, Integer faixa, String canal) {
+        this.uf = uf;
+        this.cidade = cidade;
+        this.numeroGanhadores = numeroGanhadores;
+        this.faixa = faixa;
+        this.canal = canal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Concurso getConcurso() {
+        return concurso;
+    }
+
+    public void setConcurso(Concurso concurso) {
+        this.concurso = concurso;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Integer getNumeroGanhadores() {
+        return numeroGanhadores;
+    }
+
+    public void setNumeroGanhadores(Integer numeroGanhadores) {
+        this.numeroGanhadores = numeroGanhadores;
+    }
+
+    public Integer getFaixa() {
+        return faixa;
+    }
+
+    public void setFaixa(Integer faixa) {
+        this.faixa = faixa;
+    }
+
+    public String getCanal() {
+        return canal;
+    }
+
+    public void setCanal(String canal) {
+        this.canal = canal;
+    }
+}
